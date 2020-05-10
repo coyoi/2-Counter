c49321efa563abf83244fa311bfeaa134ff13b0c
S <xyou@yahoo.com>
2020-05-09 19:38:00 -0700

    #102. Counter JS at 2:25. My console doesnt show the NodeList. ?!?

---
+++ setup/app.js
@@ -0,0 +1,8 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelector(".btn");
+
+console.log(btns);
